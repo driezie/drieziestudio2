2adc462f4dde065fb1561da61ebc37fd3a4b3836
Jelte (Driezie) <officialjustdevv@gmail.com>
2024-03-02 22:37:57 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -6,8 +6,8 @@ const app = express();
 
 // Stel ejs in als template engine
 app.set("view engine", "ejs");
-// Stel de map met ejs templates in
-app.set("views", path.join(__dirname, "views"));
+// Set the views directory
+app.set("views", path.join(__dirname, "..", "views"));
 
 // Gebruik de map 'public' voor statische resources
 app.use(express.static("public"));
